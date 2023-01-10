ebb63739d98e4654aa7328c1751646c39a889f85
Ethan Wong <ethanwong98@yahoo.ca>
2023-01-09 20:00:32 -0800

    using Template literals to consolidate code

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,12 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
-    console.log("✅Assertion Passed✅: " + actual + " === " + expected);
+    console.log("✅Assertion Passed✅: ${actual}  ===  ${expected}")
   } else {
-    console.log("⚠️Assertion Failed⚠️: " + actual + " !== " + expected);
+    console.log("⚠️Assertion Failed⚠️: ${actual} ${expected}")
   }
 };
 
 assertEqual("Lighthouse Labs ", "Bootcamp");
 assertEqual(1, 1);
-assertEqual("fun", "fun");
-assertEqual(12, 18);
+assertEqual("fun", "fun")
+assertEqual(12, 18)
