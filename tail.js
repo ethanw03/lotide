5ca96cf29088eda5599aa6f866740ba38b712cff
Ethan Wong <ethanwong98@yahoo.ca>
2023-01-16 17:14:24 -0800

    referenced test function

--- tail.js
+++ tail.js
@@ -1,26 +1,20 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed✅: ${actual}  ===  ${expected}`)
-  } else {
-    console.log(`⚠️Assertion Failed⚠️: ${actual} !==  ${expected}`)
-  }
-};
 
 const tail = function(arr) {
   let newArr = arr.slice(1, arr.length);
   return newArr;
 };
 
+module.exports = tail
 //Test Cases//
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result, ["Lighthouse", "Labs"]);
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3);
 
-const oneItem = tail(["hi"]);
-assertEqual(oneItem.length, 0);
+// const oneItem = tail(["hi"]);
+// assertEqual(oneItem.length, 0);
 
-const emptyArr = tail([]);
-assertEqual(emptyArr.length, 0);
+// const emptyArr = tail([]);
+// assertEqual(emptyArr.length, 0);
