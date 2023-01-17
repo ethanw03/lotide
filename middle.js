43851a1dc87955a6a3f226ad7a807a00674a6946
Ethan Wong <ethanwong98@yahoo.ca>
2023-01-16 19:29:20 -0800

    refactored to use unit test in test folder

--- middle.js
+++ middle.js
@@ -1,27 +1,6 @@
-let eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i <= arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false
-    }
-  } return true;
-}
-
-let assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`Assertion Passed!: ${arr1} = ${arr2}!`)
-  }
-  else {
-    console.log(`Assertion Failed!: ${arr1} != ${arr2}`)
-  }
-}
-
 let middle = function (array) {
   let evenNumber = array.length % 2 === 0;
   let median = Math.floor(array.length / 2);
-
   if (array.length <= 2) {
     return [];
   } else if (evenNumber) {
@@ -29,11 +8,6 @@ let middle = function (array) {
   } else {
     return [array[median]]
   }
-}
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5, 6])
+};
+
+module.exports = middle;
