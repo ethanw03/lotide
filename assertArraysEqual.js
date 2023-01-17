6ff4921a0364b8b3222045a4d90bcf51ca778d2e
Ethan Wong <ethanwong98@yahoo.ca>
2023-01-16 19:19:27 -0800

    refactored to add reference to unit test

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,4 @@
-let eqArrays = function(arr1, arr2){
-  if (arr1.length !== arr2.length){
-    return false;
-  }
-  for (let i = 0; i <= arr1.length; i++){
-    if (arr1[i] !== arr2[i]){
-      return false;
-    }
-  }return true;
-}
-
+const eqArrays = require('./eqArrays')
 let assertArraysEqual = function(arr1, arr2){
   if (eqArrays(arr1, arr2)){
     console.log(`Assertion Passed!: ${arr1} = ${arr2}!`)
@@ -16,5 +6,8 @@ let assertArraysEqual = function(arr1, arr2){
   else{console.log(`Assertion Failed!: ${arr1} != ${arr2}`)
 }
 }
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 4], [1, 2, 3]);
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);
+// assertArraysEqual([1, 2, 4], [1, 2, 3]);
+
+
+module.exports = assertArraysEqual
