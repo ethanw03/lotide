9993edd48bb274d552d7e2076c082f372a6387eb
Ethan Wong <ethanwong98@yahoo.ca>
2023-01-16 16:35:14 -0800

    refactored head.js to require test file

--- head.js
+++ head.js
@@ -1,16 +1,8 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed✅: ${actual}  ===  ${expected}`)
-  } else {
-    console.log(`⚠️Assertion Failed⚠️: ${actual} !==  ${expected}`)
-  }
-};
+const assertEqual = require('./assertEqual');
+
 const head = function (arr) {
   let firstElement = arr[0];
   return firstElement;
 }
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
-assertEqual(head(["Labs"]), "Hello");
+module.exports = head
